44f46beb15968be1c6ce6b522bc7828d8875d32d
jliong-ch <lewpie97@gmail.com>
2024-07-02 15:38:07 +0800

    Adding props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup /></div>
+  const items = ["New York", "San Francisco", "Tokyo", "London"];
+
+  const itemSelectHandler = (item: string) => {
+    console.log(item);
+  }
+
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" onItemSelected={itemSelectHandler} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,25 +1,29 @@
 import { useState } from "react";
 
+interface ListGroupProps {
+  items: string[];
+  heading: string;
 
-function ListGroup() {
-  const items = ["first", "second", "third", "fourth"];
-  
+  onItemSelected: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onItemSelected }: ListGroupProps) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
-
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       <ul className="list-group">
         {items.map((item, i) => (
           <li
             className={
-              selectedIndex === i ? 
-                "list-group-item active" :
-                "list-group-item"
+              selectedIndex === i ? "list-group-item active" : "list-group-item"
             }
             key={i}
-            onClick={() => setSelectedIndex(i)}
+            onClick={() => {
+              setSelectedIndex(i);
+              onItemSelected(item);
+            }}
           >
             {item}
           </li>
